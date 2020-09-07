954885d4793f5204cbaa32ce1e656b5781fee5b8
JC <jc@bali.villas>
2020-09-08 01:50:46 +0700

    Updates

---
+++ precache-manifest.e24d2dbcd9206c67b89f8ae77e54c172.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3849e6bb85fca9ad38a3902c4dcdce6",
+    "url": "/app1/index.html"
+  },
+  {
+    "revision": "0765bd32518ea056cd92",
+    "url": "/app1/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f528340de77abaf92b18",
+    "url": "/app1/static/js/2.98914163.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/app1/static/js/2.98914163.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0765bd32518ea056cd92",
+    "url": "/app1/static/js/main.e100c45d.chunk.js"
+  },
+  {
+    "revision": "87e2e68ad7b172958557",
+    "url": "/app1/static/js/runtime-main.2e62758e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/app1/static/media/logo.5d5d9eef.svg"
+  }
+]);
